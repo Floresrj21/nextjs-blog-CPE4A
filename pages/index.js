175b3576ac1190b98dc26c25593299699c3449f4
Floresrj21 <111619187+Floresrj21@users.noreply.github.com>
2022-09-17 19:47:11 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,13 +9,24 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>My name is Raphael Jacob A. Flores, My hobbies are reading, 
-        watching and gaming. My favorite food is Barbecued Pork Belly. 
-        I like the things I find interesting and make me curious about them.</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+      <b>Contact Info</b>
+      <p> Phone number: 09935250728 </p>
+      <p> Email: ralphe.jakeson@gmail.com </p>
+      <p> Address: 399 San Nicolas, City of San Fernando, Pampanga </p>
+      <b> Short Description </b>
+        <p>I am Raphael Jacob A. Flores I was born on November 2 of the year 2000 and a 21 year old male student of the University of the Assumption, BS-Cpe-4A.
+          I have many hobbies such includes reading, watching, and learning.
         </p>
+        <b>Skills</b>
+        <p> Problem solving, Coding, Technical Skills, Cooking,
+            Communication Skills, Strategic Analysis, Team Management,
+            Tool Handling, Fast Learner
+        </p>
+        <b>Education Attainment</b>
+        <p>Grade School: University of the Assumption - 2004-2013</p>
+        <p>Junior High School: University of the Assumption - 2013-2017</p>
+        <p>Senior High School: University of the Assumption, STEM strand - 2017-2019</p>
+        <p>College: University of the Assumption, College of Engineering and Architecture, BS-Cpe - 2019-present</p>
       </section>
     </Layout>
   );
